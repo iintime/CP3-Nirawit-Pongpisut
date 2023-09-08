88947f1f9e35a8396c0762657aecdcf6b51519e7
iintime <pongpisut.n@hotmail.com>
2023-09-08 14:07:50 +0700

    Added Score

---
+++ Exercise4_Nirawit_P.py
@@ -0,0 +1,9 @@
+a = 75.9
+b = 80.5
+c = 95.0
+d = 89.0
+print("---Your Score---")
+print("Foundation English :", a)
+print("General Business :" ,b)
+print("Introduction to Computer Systems :", c)
+print("Computer Programming :", d)
