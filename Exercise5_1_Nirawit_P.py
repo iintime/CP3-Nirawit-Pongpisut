ddbeb026dbad7df6a2155ed7207c01a9fb3d9331
iintime <pongpisut.n@hotmail.com>
2023-09-09 14:23:10 +0700

    Added Operation

---
+++ Exercise5_1_Nirawit_P.py
@@ -0,0 +1,12 @@
+x = int(input("Your number to Done : "))
+y = int(input("Your number to Operate: "))
+a = x+y
+b = x-y
+c = x*y
+d = x/y
+
+
+print(x, "+", y, "=", a)
+print(x, "-", y, "=", b)
+print(x, "*", y, "=", c)
+print(x, "/", y, "=", d)
