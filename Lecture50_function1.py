70ff4a6251414ad79c59b60d47f10c835eee4d67
iintime <pongpisut.n@hotmail.com>
2023-09-13 15:34:45 +0700

    added func

---
+++ Lecture50_function1.py
@@ -0,0 +1,13 @@
+def addNumberPlus(x,y):
+    print(x+y)
+def addNumberMinus(x,y):
+    print(x-y)
+def addNumberMul(x,y):
+    print(x*y)
+def addNumberDiv(x,y):
+    print(x/y)
+
+addNumberPlus(10,5)
+addNumberMinus(15,5)
+addNumberMul(20,5)
+addNumberDiv(25,5)
