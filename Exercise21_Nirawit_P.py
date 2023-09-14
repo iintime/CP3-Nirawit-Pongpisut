df652ecfb2542acf4aed35bdbd903f5aa955258f
iintime <pongpisut.n@hotmail.com>
2023-09-15 01:48:16 +0700

    Added new BMI CAL

--- Exercise21_Nirawit_P.py
+++ Exercise21_Nirawit_P.py
@@ -1,36 +1,36 @@
 from tkinter import *
 import math
-
 def leftClickButton(event):
-    print(float(textBoxWeight.get())/math.pow(float(textBoxHeight.get())/100,2))
-    bmi = float(textBoxWeight.get())/math.pow(float(textBoxHeight.get())/100,2)
-    labelBMI.configure(text=float(textBoxWeight.get())/math.pow(float(textBoxHeight.get())/100,2))
-    if bmi >= 30:
-        labelResult.configure(text="Very Fat",fg="red")
-    elif bmi >= 25.0 and bmi <= 29.9:
-        labelResult.configure(text="Fat",fg="orange")
-    elif bmi >= 23.0 and bmi <= 24.9:
-        labelResult.configure(text="Over Weight",fg="yellow")
-    elif bmi >= 18.6 and bmi <= 22.9:
-        labelResult.configure(text="Normal Weight",fg="green")
-    elif bmi <= 18.5:
-        labelResult.configure(text="Too Thin",fg="blue")
+    # labelResult.configure(text=float(textBoxWeight.get())/math.pow(float(textBoxHeight.get())/100,2))
+    height = float(textBoxHeight.get())
+    weight = float(textBoxWeight.get())
+    BMI = round((weight / (math.pow(height / 100, 2))), 2)
+    print(BMI)
+    if BMI < 18.5:
+        labelResult.configure(text=['Very Skinny'])
+    elif BMI < 22.9:
+        labelResult.configure(text=["Normal"])
+    elif BMI < 24.9:
+        labelResult.configure(text=["Overweight"])
+    elif BMI < 29.9:
+        labelResult.configure(text=["Fat"])
+    else:
+        labelResult.configure(text=["Very Fat"])
 
 
 MainWindow = Tk()
-labelHeight = Label(MainWindow, text = "Height (cm.)")
-labelHeight.grid(row=0, column=0)
+labelHeight = Label(MainWindow,text="Height (cm)")
+labelHeight.grid(row=0,column=0)
 textBoxHeight = Entry(MainWindow)
-textBoxHeight.grid(row=0, column=2)
-labelWeight = Label(MainWindow, text = "Weight (kg.)")
-labelWeight.grid(row=1, column=0)
+textBoxHeight.grid(row=0,column=1)
+labelWeight = Label(MainWindow,text="Weight (kg)")
+labelWeight.grid(row=1,column=0)
 textBoxWeight = Entry(MainWindow)
-textBoxWeight.grid(row=1, column=2)
-calculateButton = Button(MainWindow, text = "Calculation")
-calculateButton.bind("<Button-1>",leftClickButton)
+textBoxWeight.grid(row=1,column=1)
+calculateButton = Button(MainWindow,text="Result")
+calculateButton.bind('<Button-1>',leftClickButton)
 calculateButton.grid(row=2,column=0)
-labelBMI = Label(MainWindow, text="BMI")
-labelBMI.grid(row=2,column=2)
-labelResult = Label(MainWindow)
-labelResult.grid(row=3,column=1)
+labelResult = Label(MainWindow,text="Result")
+labelResult.grid(row=2,column=1)
+
 MainWindow.mainloop()
